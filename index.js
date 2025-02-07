7d0d610602ca1b826ef56807ea3ad2cac43cc4b5
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2025-02-07 10:35:39 -0700

    Rough Finished Project, add express later and fix scaling

--- index.js
+++ index.js
@@ -42,63 +42,121 @@ const weatherCodeDict = {
 };
 
 const iconDictionary = {
-	'0': '<i class="fa-solid fa-sun"></i>',
-	'1': '<i class="fa-solid fa-sun"></i>',
-	'2': '<i class="fa-solid fa-cloud-sun"></i>',
-	'3': '<i class="fa-light fa-cloud"></i>',
-	'45': '<i class="fa-light fa-cloud"></i>',
-	'48': '<i class="fa-light fa-cloud"></i>',
-	'51': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'52': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'53': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'55': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'56': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'57': '<i class="fa-thin fa-snowflake"></i>',
-	'61': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'63': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'65': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'66': '<i class="fa-thin fa-snowflake"></i>',
-	'67': '<i class="fa-thin fa-snowflake"></i>',
-	'71': '<i class="fa-thin fa-snowflake"></i>',
-	'73': '<i class="fa-thin fa-snowflake"></i>',
-	'75': '<i class="fa-thin fa-snowflake"></i>',
-	'77': '<i class="fa-thin fa-snowflake"></i>',
-	'80': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'81': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'82': '<i class="fa-solid fa-cloud-sun-rain"></i>',
-	'85': '<i class="fa-thin fa-snowflake"></i>',
-	'86': '<i class="fa-thin fa-snowflake"></i>',
-	'95': '<i class="fa-light fa-cloud-bolt"></i>',
-	'96': '<i class="fa-light fa-cloud-bolt"></i>',
-	'99': '<i class="fa-light fa-cloud-bolt"></i>'
+	'0': '☀️',
+	'1': '☀️',
+	'2': '⛅',
+	'3': '☁️',
+	'45': '🌫️',
+	'48': '🌫️',
+	'51': '🌦️',
+	'52': '🌦️',
+	'53': '🌦️',
+	'55': '🌦️❄️',
+	'56': '🌦️❄️',
+	'57': '🌦️❄️',
+	'61': '🌧️',
+	'63': '🌧️',
+	'65': '🌧️',
+	'66': '🌧️❄️',
+	'67': '🌧️❄️',
+	'71': '🌨️',
+	'73': '🌨️',
+	'75': '🌨️',
+	'77': '🌨️',
+	'80': '🌧️',
+	'81': '🌧️',
+	'82': '🌧️',
+	'85': '🌨️',
+	'86': '🌨️',
+	'95': '⛈️',
+	'96': '⛈️❄️',
+	'99': '⛈️❄️'
 };
+
+function splitInput(string) {
+	return string.split(",").map(function(item) {
+		return item.trim();
+	  });
+}
+
+//takes latitude, longitude arr of size two and sees if it is a number
+function searchIsValid(inputArr) {
+	if(isNaN(inputArr[0]) || isNaN(inputArr[1])){
+		return false;
+	}
+	return true;
+}
+
+function initializeWeather(){
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	var searchParams = "";
+
+	//set search text
+	document.getElementById('search-text').value = urlParams.get('search');
+
+	if(urlParams != null && ((document.getElementById('search-text').value) != null)){
+		searchParams = splitInput(urlParams.get('search'));
+
+		//if search is valid, update parameters
+		if(searchIsValid(searchParams)){
+			// Get the value of a specific parameter
+			userLat = searchParams[0];
+			console.log(userLat);
+		
+			userLong = searchParams[1];
+			console.log(userLong)
+		}
+		else{
+			//ADD ERROR ELEMENT LATER
+			console.log("Query empty, using default location.");
+		}
+	}
+	//otherwise use default search starting with update temperature url
+	url = "https://api.open-meteo.com/v1/forecast?latitude=" + userLat + "&longitude="+ userLong + "&current=temperature_2m,apparent_temperature,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
+
+	//update conditions url
+	conditionsURL = "https://api.open-meteo.com/v1/forecast?latitude=" + userLat + "&longitude=" + userLong + "&current=relative_humidity_2m,precipitation,weather_code,wind_gusts_10m&daily=weather_code,precipitation_sum,precipitation_probability_max";
+
+	//Updating based on which page we are on
+	if (document.title == 'Temperature') {
+		console.log("Calling update weather");
+		updateWeather();
+	}
+	else{
+		console.log("Calling update precipitation")
+		updatePrecipitation();
+	}
+}
+
+
 
 function getSearchText(){
 	const input = document.getElementById("search-text").value;
 
 	//test for value
-	//console.log(input)
+	console.log(input)
 
-	const inputArr = input.split(",").map(function(item) {
-		return item.trim();
-	  });
-
+	//split input into array
+	const inputArr = splitInput(input);
 
 	//for debugging purposes
 	console.log(inputArr);
 
 	//if the input is not a number, it can't be lat/long
-	if(isNaN(inputArr[0]) || isNaN(inputArr[1])){
-		throw new Error("Please input a number for latitude and longitude.");
-	}
-	else{
+	if(searchIsValid(inputArr)){
 		userLat = inputArr[0];
 		console.log(userLat);
 
 		userLong = inputArr[1];
 		console.log(userLong);
 	}
+	else{
+		document.getElementById("search-text").value = '';
 
+		//throw error
+		throw new Error("Please input a number for latitude and longitude.");
+	}
 
 	//update temperature url
 	url = "https://api.open-meteo.com/v1/forecast?latitude=" + userLat + "&longitude="+ userLong + "&current=temperature_2m,apparent_temperature,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min";
@@ -106,9 +164,8 @@ function getSearchText(){
 	//update conditions url
 	conditionsURL = "https://api.open-meteo.com/v1/forecast?latitude=" + userLat + "&longitude=" + userLong + "&current=relative_humidity_2m,precipitation,weather_code,wind_gusts_10m&daily=weather_code,precipitation_sum,precipitation_probability_max";
 
-	//window.location.href = "./index.html";
-
-	if (document.title == 'temperature') {
+	//Updating based on which page we are on
+	if (document.title == 'Temperature') {
 		console.log("Calling update weather");
 		updateWeather();
 
@@ -138,32 +195,37 @@ function updatePrecipitation(){
 			document.querySelector(".current-day").innerText = timestampToDay(data.daily.time[0]);
 			document.querySelector(".current-gusts").innerText = "Wind Gusts: " + data.current.wind_gusts_10m;
 			document.querySelector(".current-humidity").innerText = "Humidity: " + data.current.relative_humidity_2m;
-			//document.querySelector('.first-icon').innerHTML = changeWeatherIcon(data.daily.weather_code[0]);
+			document.getElementById('current-icon').innerText = changeWeatherIcon(data.current.weather_code);
 
 			//load the data for tomorrow
 			document.querySelector('.tomorrow').innerText = timestampToDay(data.daily.time[1]);
 			document.querySelector('.precipitation-chance').innerText = data.daily.precipitation_probability_max[1] + '%';
-			//TODO: Dynamically change icon
+			document.getElementById('second-icon').innerText = changeWeatherIcon(data.daily.weather_code[1]);
 			
 			//load the data for the third day
 			document.querySelector('.third-day').innerText = timestampToDay(data.daily.time[2]);
 			document.querySelector('.third-precipitation-chance').innerText = data.daily.precipitation_probability_max[2] + '%';
+			document.getElementById('third-icon').innerText = changeWeatherIcon(data.daily.weather_code[2]);
 		
 			//load the data for the fourth day
 			document.querySelector('.fourth-day').innerText = timestampToDay(data.daily.time[3]);
 			document.querySelector('.fourth-precipitation-chance').innerText = data.daily.precipitation_probability_max[3] + '%';
+			document.getElementById('fourth-icon').innerText = changeWeatherIcon(data.daily.weather_code[3]);
 
 			//load the data for the fifth day
 			document.querySelector('.fifth-day').innerText = timestampToDay(data.daily.time[4]);
 			document.querySelector('.fifth-precipitation-chance').innerText = data.daily.precipitation_probability_max[4] + '%';
+			document.getElementById('fifth-icon').innerText = changeWeatherIcon(data.daily.weather_code[4]);
 			
 			//load the data for the sixth day
 			document.querySelector('.sixth-day').innerText = timestampToDay(data.daily.time[5]);
 			document.querySelector('.sixth-precipitation-chance').innerText = data.daily.precipitation_probability_max[5] + '%';
+			document.getElementById('sixth-icon').innerText = changeWeatherIcon(data.daily.weather_code[5]);
 
 			//load the data for the seventh day
 			document.querySelector('.seventh-day').innerText = timestampToDay(data.daily.time[6]);
 			document.querySelector('.seventh-precipitation-chance').innerText = data.daily.precipitation_probability_max[6] + '%';
+			document.getElementById('seventh-icon').innerText = changeWeatherIcon(data.daily.weather_code[6]);
 
 			console.log(data);
 		})
@@ -171,7 +233,6 @@ function updatePrecipitation(){
 			console.error('There was an error in the fetch operation:', error);	
 		});	
 }
-
 
 function updateWeather(){
 	fetch(url).then(response =>
@@ -190,38 +251,43 @@ function updateWeather(){
 			document.querySelector(".current-apparent").innerText = 'Feels like: ' + data.current.apparent_temperature;
 			document.querySelector(".current-high").innerText = "High: " + data.daily.temperature_2m_max[0];
 			document.querySelector(".current-low").innerText = "Low: " + data.daily.temperature_2m_min[0];
-			//document.querySelector('.first-icon').innerHTML = changeWeatherIcon(data.daily.weather_code[0]);
+			document.getElementById('current-icon').innerText = changeWeatherIcon(data.current.weather_code);
 
 			//load the data for tomorrow
 			document.querySelector('.tomorrow').innerText = timestampToDay(data.daily.time[1]);
 			document.querySelector('.tomorrow-high').innerText = data.daily.temperature_2m_max[1];
 			document.querySelector('.tomorrow-low').innerText = data.daily.temperature_2m_min[1];
-			//TODO: Dynamically change icon
+			document.getElementById('second-icon').innerText = changeWeatherIcon(data.daily.weather_code[1]);
 			
 			//load the data for the third day
 			document.querySelector('.third-day').innerText = timestampToDay(data.daily.time[2]);
 			document.querySelector('.third-high').innerText = data.daily.temperature_2m_max[2];
 			document.querySelector('.third-low').innerText = data.daily.temperature_2m_min[2];
+			document.getElementById('third-icon').innerText = changeWeatherIcon(data.daily.weather_code[2]);
 
 			//load the data for the fourth day
 			document.querySelector('.fourth-day').innerText = timestampToDay(data.daily.time[3]);
 			document.querySelector('.fourth-high').innerText = data.daily.temperature_2m_max[3];
 			document.querySelector('.fourth-low').innerText = data.daily.temperature_2m_min[3];
+			document.getElementById('fourth-icon').innerText = changeWeatherIcon(data.daily.weather_code[3]);
 
 			//load the data for the fifth day
 			document.querySelector('.fifth-day').innerText = timestampToDay(data.daily.time[4]);
 			document.querySelector('.fifth-high').innerText = data.daily.temperature_2m_max[4];
 			document.querySelector('.fifth-low').innerText = data.daily.temperature_2m_min[4];
+			document.getElementById('fifth-icon').innerText = changeWeatherIcon(data.daily.weather_code[4]);
 
 			//load the data for the sixth day
 			document.querySelector('.sixth-day').innerText = timestampToDay(data.daily.time[5]);
 			document.querySelector('.sixth-high').innerText = data.daily.temperature_2m_max[5];
 			document.querySelector('.sixth-low').innerText = data.daily.temperature_2m_min[5];
+			document.getElementById('sixth-icon').innerText = changeWeatherIcon(data.daily.weather_code[5]);
 
 			//load the data for the seventh day
 			document.querySelector('.seventh-day').innerText = timestampToDay(data.daily.time[6]);
 			document.querySelector('.seventh-high').innerText = data.daily.temperature_2m_max[6];
 			document.querySelector('.seventh-low').innerText = data.daily.temperature_2m_min[6];
+			document.getElementById('seventh-icon').innerText = changeWeatherIcon(data.daily.weather_code[6]);
 
 			console.log(data);
 		})
