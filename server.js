10f0f9b6f65f2212f6ac699d631ac2617be7b42a
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2025-02-13 12:35:17 -0700

    Added express server and fixed an onload issue related to empty string parameter. It will now load the default coordinates.

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
-const PORT = process.env.PORT || 4000;
+const port = 4000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
